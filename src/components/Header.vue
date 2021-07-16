0a9d92d39f50d45f1baa9f52af80515f8e81bc2c
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-07-16 12:47:03 +0200

    INIT

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="header">
+    <Nav />
+    <Jumbotron />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped lang="scss">
+</style>
